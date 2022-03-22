4ed29562159afca93dd4b6a2b76bba4e31c64ffa
codepilot2010 <93812156+codepilot2010@users.noreply.github.com>
2022-03-22 19:02:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+
+var a = 10;
+var b = 15;
+
+var add = a+b;
+console.log(add);
+document.getElementById.innerHTML= add;
